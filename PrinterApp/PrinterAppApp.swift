848561c59c18b8281bd78aa8faa9a19038501929
natalieyaaaa <natali.takov4uk@gmail.com>
2024-03-12 19:22:42 +0200

    Initial commit

---
+++ PrinterApp/PrinterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterAppApp.swift
+//  PrinterApp
+//
+//  Created by Наташа Яковчук on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrinterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
